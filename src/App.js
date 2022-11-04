82197e5a81dcb87919d79f30b671137ab7babf81
Edwin Laurent <laurent.edwin@icloud.com>
2022-11-04 22:04:22 +0100

    set carrousel width on mobile

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 // Import components
 import Header from "./layouts/Header/Header";
 import Footer from "./layouts/Footer/Footer";
-//Import styles
-import styles from "./App.css";
-
+import "./App.css";
 //Import routers
 import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
       <div className="content">
         <Outlet />
